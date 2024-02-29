b6de0182a142b71918ff2984f6c749997c8e1d69
bajradhyan <37986460+bajradhyan@users.noreply.github.com>
2024-02-28 23:49:48 -0500

    all the repository added

---
+++ Project1/src/pack2/SampleClass.java
@@ -0,0 +1,14 @@
+package pack2;
+
+public class SampleClass {
+
+
+	int x=100,y=200,z=300;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("iam first");
+		System.out.println("iam second");
+	}
+
+}
